132bd0c611fae777385699f5b618741982099848
chen guang <cgcgbcbc@163.com>
2013-02-05 21:57:38 +0800

    2013/2/5

--- gen/R.java.d
+++ gen/R.java.d
@@ -1,12 +1,12 @@
-D:\transportation-competition\gen\com\thu\tdnav\R.java \
- : D:\transportation-competition\res\drawable-hdpi\ic_launcher.png \
-D:\transportation-competition\res\drawable-ldpi\ic_launcher.png \
-D:\transportation-competition\res\drawable-mdpi\ic_launcher.png \
-D:\transportation-competition\res\drawable-xhdpi\ic_launcher.png \
-D:\transportation-competition\res\layout\main.xml \
-D:\transportation-competition\res\values\strings.xml \
-D:\transportation-competition\bin\res\drawable-hdpi\ic_launcher.png \
-D:\transportation-competition\bin\res\drawable-ldpi\ic_launcher.png \
-D:\transportation-competition\bin\res\drawable-mdpi\ic_launcher.png \
-D:\transportation-competition\bin\res\drawable-xhdpi\ic_launcher.png \
-D:\transportation-competition\bin\AndroidManifest.xml \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\gen\com\thu\tdnav\R.java \
+ : D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\res\drawable-hdpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\res\drawable-ldpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\res\drawable-mdpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\res\drawable-xhdpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\res\layout\main.xml \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\res\values\strings.xml \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\bin\res\drawable-hdpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\bin\res\drawable-ldpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\bin\res\drawable-mdpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\bin\res\drawable-xhdpi\ic_launcher.png \
+D:\appdata\android\tsinghua-transportation-competition-indoor-navigation\bin\AndroidManifest.xml \
--- src/com/thu/tdnav/MainActivity.java
+++ src/com/thu/tdnav/MainActivity.java
@@ -1,7 +1,9 @@
 package com.thu.tdnav;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -9,31 +11,46 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);	
+    }
+    
+    //debug dialog
+    public void showDebugDialog()
+    {
+  		AlertDialog builder = new AlertDialog.Builder(this)
+  		.setTitle("notice")
+  		.setMessage("code uncompleted")
+  		.setPositiveButton("I know",null)
+  		.show();
     }
 	
-	//二维码查询
+	//2d query
 	public void inputby2d(View view)
 	{
+		showDebugDialog();
 	}
 	
-	//出发地语音查询
+	//start place voice query
 	public void inputbyvoice_start(View view)
 	{
+		showDebugDialog();
 	}
 	
-	//目的地语音查询
+	//destination voice query
 	public void inputbyvoice_end(View view)
 	{
+		showDebugDialog();
 	}
 	
-	//菜单
+	//menu
 	public void menuonclick(View view)
 	{
+		showDebugDialog();
 	}
 	
-	//搜索
+	//search
 	public void searchonclick(View view)
 	{
+		showDebugDialog();
 	}
 }
